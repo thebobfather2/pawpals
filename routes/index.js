b7844c53cdc521238b208cd640ebd947ef32cc98
larigens@gmail.com <larigens@gmail.com>
2023-02-06 20:09:18 -0500

    added starter files/foldersand some code

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+// Prefix all routes defined in the api directory with `/api`.
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.send("<h1>Ooops</h1>")
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-import 
+// Packages / Dependencies.
+const express = require("express");
+const routes = require("./routes");
+const sequelize = require("./config/connection.js"); // Imports sequelize connection.
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // Recognizes the incoming Request Object as a JSON Object.
+
+// Turn on routes.
+app.use(routes);
+
+// Default response for any other request (Not Found).
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// Syncs sequelize models to the database, then starts the Express.js server.
+sequelize.sync({ force: false }).then(() => {
+  // Force false so data doesn't get dropped on every sync.
+  app.listen(PORT, () => console.log(`Now listening on Port ${PORT}`));
+});
