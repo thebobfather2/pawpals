9d53d8fca6737968b201f794c715bf2d393a2fa4
Anastasiia Markova <anastasia19markova@gmail.com>
2023-02-08 11:37:50 -0500

    models file added and api

---
+++ models/users.js
@@ -0,0 +1,79 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+// create user table to house user information
+class Users extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.passwordHash)
+    }
+}
+
+Users.init(
+    {
+        userId: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+            validate: {
+                len: [5]
+            }
+        },
+
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [5]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        passwordHash: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+        },
+        registeredAt: {
+            type: DataTypes.DATE(),
+            allowNull: true,
+
+        },
+        lastLogin: {
+            type: DataTypes.DATE(),
+            allowNull: true,
+        },
+        profile: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        }
+    },
+    {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.passwordHash = await bcrypt.hash(newUserData.passwordHash, 10);
+                return newUserData;
+            },
+
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.passwordHash = await bcrypt.hash(updatedUserData.passwordHash, 10);
+                return updatedUserData;
+            }
+        },
+
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        initialAutoIncrement: 10230,
+        modelName: 'users'
+    }
+);
+
+module.exports = Users;
---
+++ seeds/index.js
@@ -0,0 +1,23 @@
+const seedPets = require('./pets-seed');
+const seedUsers = require('./users-seed');
+const seedLikes = require('./likes-seed');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+    await sequelize.sync({ force: true });
+    console.log('\n----- DATABASE SYNCED -----\n');
+
+    await seedPets();
+    console.log('\n----- PETS SEEDED -----\n');
+
+    await seedUsers();
+    console.log('\n----- USERS SEEDED -----\n');
+
+    await seedLikes();
+    console.log('\n----- LIKES SEEDED -----\n');
+
+    process.exit(0);
+};
+
+seedAll();
